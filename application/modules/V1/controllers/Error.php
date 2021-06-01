8a4201a5fe47798c39e5787833fa94becf2e355f
Silie <1312119468@qq.com>
2021-06-01 10:54:54 +0800

    修复日志错误

--- application/modules/V1/controllers/Error.php
+++ application/modules/V1/controllers/Error.php
@@ -10,17 +10,18 @@ class ErrorController extends Yaf\Controller_Abstract {
     //从2.1开始, errorAction支持直接通过参数获取异常
     public function errorAction($exception) {
         //1. assign to view engine
-        $context = array(
+	$context = array(
             'msg' => str_replace(['|',"\n"],'',$exception->getMessage() . ' ---file:'.$exception->getFile().' ---lines:'.$exception->getLine()),
-            'level' => $exception->getSeverity(),
+            'level' => method_exists($exception,'getSeverity')?$exception->getSeverity():5,
             'model' => defined('MODULE_NAME') ? $this->_request->module:'',
             'controller' => defined('CONTROLLER_NAME') ? $this->_request->controller:'',
             'action' => defined('ACTION_NAME') ? $this->_request->action:'',
         );
-        \SeasLog::log(SEASLOG_ERROR,'ERROR','{msg} | {level} | {model} | {controller} | {action}',$context);
+        \SeasLog::log(SEASLOG_ERROR,'ERROR {msg} | {level} | {model} | {controller} | {action}',$context);
         echo "错误已经记录到SeasLog";
         return false;
-//        $this->getView()->assign("exception", $exception);
+    
+	//$this->getView()->assign("exception", $exception);
         //5. render by Yaf
     }
 }
