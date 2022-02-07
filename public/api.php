1fb881c6eb97d900feab0d6ede8c4030a4849675
撕裂的伤痕 <38691833+silie666@users.noreply.github.com>
2022-02-07 10:51:40 +0800

    Update api.php

--- public/api.php
+++ public/api.php
@@ -1,8 +1,6 @@
 <?php
 header('content-type:text/html;charset=utf-8');
 /* 定义这个常量是为了在application.ini中引用*/
-define('APP_DEBUG',true);
-define("MAGIC_QUOTES_GPC",  false);
 define('PROJECT_PATH', dirname(__FILE__));
 define('APPLICATION_PATH', PROJECT_PATH."/..");
 
